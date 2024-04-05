bad74f1c9d242a9f5db0518d998efb82bf4d039b
kei ito <ito.kei@jvckenwood.com>
2024-04-05 10:50:01 +0900

    固定ヘッダー対応

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -16,9 +16,36 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Item> Items { get; set; }
+
         public MainWindow()
         {
+            Items = [
+                new("AAAAAA", 1, "aaaaaa"),
+                new("AAAAAA", 2, "bbbb"),
+                new("AAAAAA", 3, "ccc"),
+                new("AAAAAA", 4, "ddd"),
+                new("AAAAAA", 5, "eeee"),
+                new("BBBBB", 6, "ffff"),
+                new("BBBBB", 7, "hhhh"),
+                new("BBBBB", 8, "i"),
+                new("BBBBB", 9, "jjjj"),
+                new("BBBBB", 10, "kkkk"),
+                new("BBBBB", 11, "lllllllll"),
+                new("BBBBB", 12, "mmmmmmmmmm"),
+                new("BBBBB", 13, "nnn"),
+                new("BBBBB", 14, "ooooooooooo"),
+                new("BBBBB", 15, "pp"),
+                new("BBBBB", 16, "qqqqqqqqqq"),
+                new("CCCCCCCC", 17, "rrrr"),
+                new("CCCCCCCC", 18, "sss"),
+                new("CCCCCCCC", 19, "ttt"),
+                new("CCCCCCCC", 20, "uuuuuuuuuuuuuuu"),
+                ];
+
             InitializeComponent();
         }
     }
+
+    public record Item(string GroupName, int Identifier, string Name);
 }
---
+++ WpfApp1/WpfLibrary1/Behaviors/GroupHeaderFrozenBehavior.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.Xaml.Behaviors;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+using WpfLibrary1.Controls;
+using WpfLibrary1.Utils;
+
+namespace WpfLibrary1.Behaviors
+{
+    /// <summary>
+    /// GroupItemのヘッダを固定表示するBehavior
+    /// </summary>
+    public class GroupHeaderFrozenBehavior : Behavior<ItemsControl>
+    {
+        private static readonly Dictionary<GroupItem, WeakReference<HeaderAdorner>> _CurrentGroupItem = new Dictionary<GroupItem, WeakReference<HeaderAdorner>>();
+
+        #region HeaderTemplate依存関係プロパティ
+        public DataTemplate HeaderTemplate
+        {
+            get { return (DataTemplate)GetValue(HeaderTemplateProperty); }
+            set { SetValue(HeaderTemplateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HeaderTemplate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderTemplateProperty =
+            DependencyProperty.Register("HeaderTemplate", typeof(DataTemplate), typeof(GroupHeaderFrozenBehavior), new PropertyMetadata(null));
+        #endregion
+
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.Loaded += AssociatedObject_Loaded;
+            AssociatedObject.Unloaded += AssociatedObject_Unloaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.Loaded -= AssociatedObject_Loaded;
+            AssociatedObject.Unloaded -= AssociatedObject_Unloaded;
+        }
+
+        private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetScrollChangedEvent(true);
+        }
+
+        private void AssociatedObject_Unloaded(object sender, RoutedEventArgs e)
+        {
+            SetScrollChangedEvent(false);
+        }
+
+        private void SetScrollChangedEvent(bool add)
+        {
+            ScrollViewer scrollViewer;
+
+            if (AssociatedObject is ListBox)
+            {
+                // リストボックス内のScrollViewerを探す
+                scrollViewer = AssociatedObject.FindChild<ScrollViewer>();
+            }
+            else
+            {
+                // 親方向にScrollViewerを探す
+                scrollViewer = AssociatedObject.FindParent<ScrollViewer>();
+            }
+
+            if (scrollViewer != null)
+            {
+                if (add)
+                {
+                    scrollViewer.ScrollChanged += ScrollViewer_ScrollChanged;
+                }
+                else
+                {
+                    scrollViewer.ScrollChanged -= ScrollViewer_ScrollChanged;
+                }
+            }
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var scrollViewer = (ScrollViewer)sender;
+
+            // ScrollViewerの描画サイズ
+            var scrollViewerRectangle = new Rect(new Point(0, 0), scrollViewer.RenderSize);
+
+            // ItemsControlのGroupItem
+            foreach (var containerItem in AssociatedObject.ItemContainerGenerator.Items)
+            {
+                // GroupItemコントロールを取得
+                var groupItemContainer = AssociatedObject.ItemContainerGenerator.ContainerFromItem(containerItem) as GroupItem;
+                if (groupItemContainer == null)
+                {
+                    Debug.WriteLine("Failed: get groupItemContainer");
+                    return;
+                }
+
+                // ScrollViewerを基準とした描画位置を計算
+                var transform = groupItemContainer.TransformToAncestor(scrollViewer);
+                var groupItemRect = transform.TransformBounds(new Rect(new Point(0, 0), groupItemContainer.RenderSize));
+
+                // ScrollViewerとGroupItemの重なりを確認
+                var intersectRect = Rect.Intersect(scrollViewerRectangle, groupItemRect);
+
+                // ヘッダー固定表示用のAdornerを表示する必要があるかどうか
+                var needDisplayAdorner = true;
+
+                // ScrollViewerの描画エリア内にGroupItemが配置されている
+                needDisplayAdorner &= intersectRect != Rect.Empty;
+
+                // かつ、GroupItemの上端がScrollViewerの上端よりも上に存在
+                needDisplayAdorner &= groupItemRect.Top <= 0;
+
+                // GroupItemのAdornerLayerを取得
+                var adornerLayer = AdornerLayer.GetAdornerLayer(groupItemContainer);
+                if (adornerLayer == null)
+                {
+                    Debug.WriteLine("Failed: get adornerLayer of GroupItem");
+                    return;
+                }
+
+                // Adornerの表示が必要な場合
+                if (needDisplayAdorner)
+                {
+                    // すでにAdornerを作成している場合はその位置を更新するだけ
+                    var headerAdorner = GetAdorner(groupItemContainer);
+                    if (headerAdorner != null)
+                    {
+                        headerAdorner.UpdateLocation(groupItemRect.Top);
+                        return;
+                    }
+
+                    // 未作成の場合は新規にAdornerを作成し、Dictionaryに加える
+                    var adorner = new HeaderAdorner(groupItemContainer)
+                    {
+                        DataContext = containerItem,
+                        HeaderTemplate = HeaderTemplate,
+                        Top = Math.Abs(groupItemRect.Top)
+                    };
+                    adornerLayer.Add(adorner);
+
+                    _CurrentGroupItem.Add(groupItemContainer, new WeakReference<HeaderAdorner>(adorner));
+                }
+                // Adornerの表示が不要な場合
+                else
+                {
+                    // AdornerをAdornerLayerとDictionaryから除去
+                    var adorner = GetAdorner(groupItemContainer);
+                    if (adorner != null)
+                    {
+                        adornerLayer.Remove(adorner);
+                        _CurrentGroupItem.Remove(groupItemContainer);
+                    }
+                }
+            }
+
+        }
+
+
+        private HeaderAdorner GetAdorner(GroupItem container)
+        {
+            if (_CurrentGroupItem.ContainsKey(container))
+            {
+                HeaderAdorner adorner;
+                if (_CurrentGroupItem[container].TryGetTarget(out adorner))
+                {
+                    return adorner;
+                }
+            }
+
+            return null;
+        }
+    }
+}
